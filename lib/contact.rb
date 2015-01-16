c403931e94b8ae30edb3e81284cdfe4a32cf0677
Kameron Johnson <kameronjohnson@gmail.com>
2015-01-16 10:23:24 -0800

    Add additional specs and methods to lib's contact file

--- lib/contact.rb
+++ lib/contact.rb
@@ -4,6 +4,7 @@ class Contact
   define_method(:initialize) do |contact_name, phone_number|
     @contact_name = contact_name
     @phone_number = phone_number
+    @id = @@contacts.length().+(1)
   end
 
   define_method(:contact_name) do
@@ -14,12 +15,30 @@ class Contact
     @phone_number
   end
 
+  define_method(:id) do
+    @id
+  end
+
   define_singleton_method(:all) do
     @@contacts
   end
 
   define_method(:save) do
     @@contacts.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@contacts = []
+  end
+
+  define_singleton_method(:find_contact) do |searched_contact|
+    found_contact = nil
+    @@contacts.each() do |contact_in_class|
+      if contact_in_class.id() == searched_contact
+        found_contact = contact_in_class
+      end
+    end
+    found_contact
   end
 
 end #end class
--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -3,6 +3,10 @@ require('contact')
 
 describe(Contact) do
 
+  before() do
+    Contact.clear()
+  end
+
   describe(".all") do
     it("is empty at first") do
       expect(Contact.all()).to(eq([]))
@@ -29,6 +33,31 @@ describe(Contact) do
       test_phone_number = Contact.new("Billy Jean", "503-555-1111")
       test_phone_number.save()
       expect(test_phone_number.phone_number()).to(eq("503-555-1111"))
+    end
+  end
+
+  describe("#id") do
+    it("gives an id for each contact") do
+      test_contact1 = Contact.new("Billy Jean", "503-555-1111")
+      expect(test_contact1.id()).to(eq(1))
+    end
+  end
+
+  describe(".clear") do
+    it("empties out all of the saved contacts") do
+      Contact.new("Billy Jean", "503-555-1111").save()
+      Contact.clear()
+      expect(Contact.all()).to(eq([]))
+    end
+  end
+
+  describe(".find_contact") do
+    it("finds the contact number by id") do
+      test_contact1 = Contact.new("Billy Jean", "503-555-1111")
+      test_contact1.save()
+      test_contact2 = Contact.new("Mary Sue", "503-555-2222")
+      test_contact2.save()
+      expect(Contact.find_contact(test_contact1.id())).to(eq(test_contact1))
     end
   end
 
